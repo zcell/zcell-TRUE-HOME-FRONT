a0f054ca0a00c7e913fc33cba3b4f1704cc9d427
nikola <wozli_23@mail.ru>
2020-06-15 21:46:40 +0500

    metrika

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="wrapper" id="wrapper">
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(64898518, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true,
+            ecommerce:"dataLayer"
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/64898518" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
     <appHeader />
     <div class="wrapper__content" id="wrapper__content">
 
